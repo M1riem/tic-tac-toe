ee8da1a30dc3ec79f11e851ea20c5f623495e6f5
M1riem <39156128+M1riem@users.noreply.github.com>
2024-03-25 19:59:46 +0700

    Added check for winnings
    Added a check to see if one of the players has won. All sides and diagonals around the player's chosen cell are checked for compliance with the player's sign. If the previous (or next) cell was also chosen by the player, then it is checked in a reversible loop until the number of cells passed is equal to the maximum. If the number of squares passed is the maximum, then this player wins.

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ for(let i = 0; i < max; i++ ) {
 //field.forEach((element) => console.log(element) );
 
 let number = 0;
-class GroupHorizontal
+class Group
 {
 	constructor(j,i) {
 		this.j = j;
@@ -22,63 +22,81 @@ class GroupHorizontal
 		this.prev = null;
 		this.head = this;
 		number++;
-		this.checkWin();
+		//this.checkWin();
 	}
 	
-	addNext(j,i) {
+	addNext(j, i, sj, si) {
 		console.log(`addNext`);
-		console.log(`now  -> j = ${this.j}, i = ${this.i}, next = ${this.next}, prev = ${this.prev}, value = ${field[this.j][this.i]}`);
-		console.log(`next -> j = ${this.j}, i+1 = ${this.i+1}, value = ${field[this.j][this.i+1]}`);
-		
-		if ( (i+1) >= 0) {console.log(`Conditions done i+1 = ${this.i + 1}>=0`); }
-			else{console.log(`Conditions fail i+1 = ${this.i + 1}<0`);}
-		if ( field[j][i] == field[j][i+1] ) {console.log(`Conditions done '${field[this.j][this.i+1]}' == '${field[this.j][this.i]}'`); }
-			else {console.log(`Conditions fail '${field[this.j][this.i+1]}' != '${field[this.j][this.i]}'`); }
-		
-		if ( ( (i+1) < max) && ( field[j][i] == field[j][i+1] ) ) {
-			console.log(`Conditions done`);
-			let newElement = new GroupHorizontal(j, i+1);
-			this.head.next = newElement;
-			newElement.prev = this.head;
-			this.head = newElement;
-			newElement.say();
-			this.say();
-			this.head.addNext(j, i+1);
+		if (!this.checkInput(sj,si)) return;
+		if (! ( ((i+si) < max) && ((j+sj) < max) && ((i+si) >=0 ) ) ) {
+			console.log(`Conditions fail (j+${sj} = ${this.j+sj} >= ${max}) || (i+${si} = (${this.i+si} < 0)) || (${this.i+si} >= ${max})`);
+			return;
 		}
+
+		console.log(`now  -> j = ${this.j}, i = ${this.i}, next = ${this.next}, prev = ${this.prev}, value = ${field[this.j][this.i]}`);
+		console.log(`next -> j = ${this.j + sj}, i = ${this.i+si}, value = ${field[this.j + sj][this.i + si]}`);
+		console.log(`Conditions done (j+${sj} = ${this.j+sj} < ${max}) || (i+${si} = (0 <= ${this.i+si} < ${max})) '`); 
+		
+		let newElement = new Group(j+sj, i+si);
+		this.head.next = newElement;
+		newElement.prev = this.head;
+		this.head = newElement;
+		//newElement.say();
+		if (this.checkWin()) return;
+		//this.say();
+		this.head.addNext(j+sj, i+si, sj, si);
 		console.log(`recursion addNext done`);
 	}
 	
-	addPrev(j,i) {
+	addPrev(j, i, sj, si) {	
 		console.log(`addPrev`);
-		console.log(`now  -> j = ${this.j}, i = ${this.i}, next = ${this.next}, prev = ${this.prev}, value = ${field[this.j][this.i]}`);
-		console.log(`prev -> j = ${this.j}, i-1 = ${this.i - 1}, value = ${field[this.j][this.i-1]}`);
-		
-		if ( (i-1) >= 0) {console.log(`Conditions done i-1 = ${this.i - 1}>=0`); }
-			else{console.log(`Conditions fail i-1 = ${this.i - 1}<0`);}
-		if ( field[j][i] == field[j][i-1] ) {console.log(`Conditions done '${field[this.j][this.i-1]}' == '${field[this.j][this.i]}'`); }
-			else {console.log(`Conditions fail '${field[this.j][this.i-1]}' != '${field[this.j][this.i]}'`); }
-		
-		if ( ( (i-1) >= 0) && ( field[j][i] == field[j][i-1] ) ) {
-			console.log(`Conditions done`);
-			let newElement = new GroupHorizontal(j, i-1);
-			this.head.prev = newElement;
-			newElement.next = this.head;
-			this.head = newElement;
-			newElement.say();
-			this.say();
-			this.head.addPrev(j, i-1);
+		// console.log(`sj = ${sj}, si = ${si}`);
+		if (!this.checkInput(sj,si)) return;
+		if (!( ((i+si) >= 0) && ((j+sj) >= 0) && ((i+si) < max) )) {
+			console.log(`Conditions fail (j+${sj} = ${this.j+sj} < 0}) || ((i+${si} = ${this.i+si} < 0) || (${this.i+si} <= ${max}))`);
+			return;
 		}
+		
+		console.log(`now  -> j = ${this.j}, i = ${this.i}, next = ${this.next}, prev = ${this.prev}, value = ${field[this.j][this.i]}`);
+		console.log(`prev -> j = ${this.j + sj}, i = ${this.i+si}, value = ${field[this.j + sj][this.i + si]}`);	
+		console.log(`Conditions done ( j+${sj} = ${this.j+sj} >= 0 ) || ( i+${si} = ( 0 <= ${this.i+si} < ${max}) )`); 
+		
+		//console.log(`Conditions done`);
+		let newElement = new Group(j+sj, i+si);
+		this.head.prev = newElement;
+		newElement.next = this.head;
+		this.head = newElement;
+		//newElement.say();
+		if (this.checkWin()) return;
+		//this.say();
+		this.head.addPrev(j+sj, i+si, sj, si);
 		console.log(`recursion addPrev done`);
+	}
+	
+	checkInput(sj, si) {
+		console.log(`checkInput -> field[${this.j+sj}][${this.i + si}]`);
+		if ( field[this.j + sj] == undefined ) {
+			console.log(`checkInput -> Conditions fail -> field[${this.j+sj}] = ${field[this.j + sj]}`);
+			return false;
+		}
+		if ( field[this.j][this.i] != field[this.j+sj][this.i+si] ) {
+			console.log(`checkInput -> Conditions fail -> field[${this.j+sj}][${this.i + si}] !=field[${this.j}][${this.i}] => ${field[this.j + sj][this.i + si]} != ${field[this.j][this.i]} `);
+			return false;
+		}
+		return true;
 	}
 	checkWin() {
 		if (number == max) {
 			alert(`Win ${field[this.j][this.i]}!`);
+			console.log(`Win ${field[this.j][this.i]}!`);
 			endGame=true;
+			return true;
 		}
+		else return false;
 	}
 
 	say() {
-		console.log(`j = ${this.j}, i = ${this.i}, next = ${this.next}, prev = ${this.prev}, value = ${field[this.j][this.i]}, amount = ${number}`);
+		console.log(`j = ${this.j}, i = ${this.i}, next = ${this.next}, prev = ${this.prev}, value = ${field[this.j][this.i]}, number = ${number}`);
 	}
 	
 }
@@ -92,12 +110,46 @@ document.getElementById("canvas").addEventListener("mousedown", function (e) {
 	if (field[j][i] != -1) return;
 	field[j][i] = gamer;
 	
-	let horizontal = new GroupHorizontal( j, i);
-	//horizontal.say();
-	horizontal.addNext(j,i);
-	horizontal.addPrev(j,i);
-	console.log("-------------------------------------------------------------------------------------------------");
-	number = 0;
+	console.log("------------------------------------horizontal---------------------------------------------------");
+	if (number != 3) { 
+		number = 0;
+		let horizontal = new Group( j, i );
+		horizontal.say();
+		let hx = 1, hy = 0;
+		if (number < 3) horizontal.addNext(j, i, hy, hx); 
+		if (number < 3) horizontal.addPrev(j, i, hy, -hx);
+	}
+	console.log("-------------------------------------vertical----------------------------------------------------");
+	if (number != 3) { 
+		number = 0;
+		let vertical = new Group( j, i );
+		vertical.say();
+		let vx = 0, vy = 1;
+		//console.log(`vx = ${vx}, vy = ${vy}`);
+		if (number < 3) vertical.addPrev(j, i, -vy, vx);
+		if (number < 3) vertical.addNext(j, i, vy, vx); 
+	}	
+	console.log("-------------------------------------diagonal/---------------------------------------------------");
+	if (number != 3) { 
+		number = 0;
+		let diagonal = new Group( j, i );
+		diagonal.say();
+		let dx = -1, dy = 1;
+		//console.log(`dx = ${dx}, dy = ${dy}`);
+		if (number < 3) diagonal.addPrev(j, i, -dy, -dx);
+		if (number < 3) diagonal.addNext(j, i, dy, dx); 
+	}
+	console.log("-------------------------------------diagonal\---------------------------------------------------");
+	if (number != 3) { 
+		number = 0;
+		let diagonal_ = new Group( j, i );
+		diagonal_.say();
+		let d_x = 1, d_y = 1;
+		//console.log(`d_x = ${d_x}, d_y = ${d_y}`);
+		if (number < 3) diagonal_.addPrev(j, i, -d_y, -d_x);
+		if (number < 3) diagonal_.addNext(j, i, d_y, d_x); 
+	}
+	console.log("-------------------------------------------------------------------------------------------------");	
 	
 	if (gamer == 'X') {
 		drawX(i*delta + Math.ceil(delta/2), j*delta + Math.ceil(delta/2), Math.ceil(delta/2) - Math.ceil(delta/4));
@@ -107,32 +159,21 @@ document.getElementById("canvas").addEventListener("mousedown", function (e) {
 		drawO(i*delta + Math.ceil(delta/2), j*delta + Math.ceil(delta/2), Math.ceil(delta/2) - Math.floor(delta/6));
 		gamer = 'X';
 	}
+	
 } );
 
-/*document.getElementById("canvas").addEventListener("mousedown", function (e) {
-	if ( endGame ) return; 
-	let coords = getRelativeCoordinates(e, canvas);
-	let i = Math.floor( coords.x/delta );
-	let j = Math.floor( coords.y/delta );
-	
-	if (field[j][i] != -1) return;
-		//console.log(`i:${i}, j:${j} <=> field[${i}][${j}] = ${field[i][j]} <=> X: ${coords.x}, Y: ${coords.y}`);
-		
-	field[j][i] = gamer;
-	turn++;
-	if (turn >= 5)  checkWinner();
-	
-	if (gamer == 'X') {
-		drawX(i*delta + Math.ceil(delta/2), j*delta+ Math.ceil(delta/2), Math.ceil(delta/2) - Math.ceil(delta/4));
-		gamer = 'O';
-	}
-	else{
-		drawO(i*delta + Math.ceil(delta/2), j*delta + Math.ceil(delta/2), Math.ceil(delta/2) - Math.floor(delta/6));
-		gamer = 'X';
-	}
-	//console.log(`i:${i}, j:${j} <=> X: ${coords.x}, Y: ${coords.y}`);
-} );*/
-	
+// function addTurn( j, i, sj, si ) {
+	// if (number != 3) { 
+		// number = 0;
+		// let turn = new Group( j, i );
+		// turn.say();
+		// let d_x = 1, d_y = 1;
+		// //console.log(`d_x = ${d_x}, d_y = ${d_y}`);
+		// if (number < 3) diagonal_.addPrev(j, i, -d_y, -d_x);
+		// if (number < 3) diagonal_.addNext(j, i, d_y, d_x); 
+	// }
+// }
+
 function checkWinner() {
 	let win = 0;
 	//1
